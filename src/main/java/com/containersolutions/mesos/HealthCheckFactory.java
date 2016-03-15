cd88846f1f69323a674a822c853a43f0f89a9849
Phil Winder <phil@winderresearch.com>
2016-03-15 09:02:17 +0000

    Prevent NPE when no healthcheck is supplied. Altered kibana properties.

--- src/main/java/com/containersolutions/mesos/HealthCheckFactory.java
+++ src/main/java/com/containersolutions/mesos/HealthCheckFactory.java
@@ -15,16 +15,20 @@ public class HealthCheckFactory implements MesosProtoFactory<Protos.HealthCheck>
     public Protos.HealthCheck create(List<Protos.Resource> resources) {
         // Dev note: HealthCheck does not work in 0.25: https://issues.apache.org/jira/browse/MESOS-3738
         Protos.HealthCheck healthDefault = Protos.HealthCheck.getDefaultInstance();
-        return Protos.HealthCheck.newBuilder()
-                .setCommand(
-                        Protos.CommandInfo.newBuilder()
-                                .setValue(mesosConfig.getHealthCheck().getOrDefault("command", "echo Success"))
-                )
-                .setTimeoutSeconds(Double.valueOf(mesosConfig.getHealthCheck().getOrDefault("timeout", Double.toString(healthDefault.getTimeoutSeconds()))))
-                .setDelaySeconds(Double.valueOf(mesosConfig.getHealthCheck().getOrDefault("delay", Double.toString(healthDefault.getDelaySeconds()))))
-                .setConsecutiveFailures(Integer.valueOf(mesosConfig.getHealthCheck().getOrDefault("consecutiveFailures", Integer.toString(healthDefault.getConsecutiveFailures()))))
-                .setGracePeriodSeconds(Double.valueOf(mesosConfig.getHealthCheck().getOrDefault("gracePeriod", Double.toString(healthDefault.getGracePeriodSeconds()))))
-                .setIntervalSeconds(Double.valueOf(mesosConfig.getHealthCheck().getOrDefault("interval", Double.toString(healthDefault.getIntervalSeconds()))))
-                .build();
+        if (mesosConfig.getHealthCheck() == null) {
+            return healthDefault;
+        } else {
+            return Protos.HealthCheck.newBuilder()
+                    .setCommand(
+                            Protos.CommandInfo.newBuilder()
+                                    .setValue(mesosConfig.getHealthCheck().getOrDefault("command", "echo Success"))
+                    )
+                    .setTimeoutSeconds(Double.valueOf(mesosConfig.getHealthCheck().getOrDefault("timeout", Double.toString(healthDefault.getTimeoutSeconds()))))
+                    .setDelaySeconds(Double.valueOf(mesosConfig.getHealthCheck().getOrDefault("delay", Double.toString(healthDefault.getDelaySeconds()))))
+                    .setConsecutiveFailures(Integer.valueOf(mesosConfig.getHealthCheck().getOrDefault("consecutiveFailures", Integer.toString(healthDefault.getConsecutiveFailures()))))
+                    .setGracePeriodSeconds(Double.valueOf(mesosConfig.getHealthCheck().getOrDefault("gracePeriod", Double.toString(healthDefault.getGracePeriodSeconds()))))
+                    .setIntervalSeconds(Double.valueOf(mesosConfig.getHealthCheck().getOrDefault("interval", Double.toString(healthDefault.getIntervalSeconds()))))
+                    .build();
+        }
     }
 }
